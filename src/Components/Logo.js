2a409fa9ea85cc70b8940e2e4dd9d5c6e679b7c9
Joshua Alphonse <joshalphonse@gmail.com>
2019-04-04 20:18:28 -0400

    working app thursday week1

---
+++ src/Components/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <img
+      className="Logos"
+      alt=""
+      src={
+        "https://cdn-images-1.medium.com/max/2600/1*8BCrOic8jIpo284ikA7ZYw.png"
+      }
+    />
+  );
+};
+export default Logo;
