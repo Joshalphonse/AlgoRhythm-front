6d04c2d7c13b1ff618d832b1f010a3a42dea0008
Joshua Alphonse <joshalphonse@gmail.com>
2019-06-06 22:31:55 -0400

    Change in HomePage

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useSpring, animated } from "react-spring";
+import { Link, withRouter } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <div>Hello, {this.props.user.username}</div>
+        <div className="hello">Hello, {this.props.user.username}</div>
+        <h1 className="hello">Welcome To AlgoRhythm</h1>
+
+        <Link to="/daw" className="workstation">
+          <button className="button-btn">Navigate To WorkStation</button>
+        </Link>
       </div>
     );
   }
